e33b77add3bd06fc1aec7443bfb0465846b7bbb6
yanrodgues <yandeassis@pm.me>
2023-03-30 18:18:03 -0300

    Add computer choice logic

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
+let playerSelection = 0;
+
+let computerSelection = 0;
+
+let playerScore = 0;
+
+let computerScore = 0;
+
+let seedComputer = 0;
+
+let rock = 1
+
+let paper = 2
+
+let scissors = 3
+
 // function called getComputerChoice
+function getComputerChoice() {
+    seedComputer = Math.floor(Math.random() * 100)
+    if (seedComputer <= 33){
+        computerSelection = rock
+    }else if (seedComputer <= 66){
+        computerSelection = paper
+    }else if (seedComputer >= 66){
+        computerSelection = scissors
+    }
+}
+
+getComputerChoice()
+
+console.log(computerSelection);
 /// Get random choice between rock paper scissors
 
-// function called playRound 
+// function called playRound
 // takes playerSelection and computerSelection, and returns a string with who won
 
 // playerSellection must accept inputs case insensitively
