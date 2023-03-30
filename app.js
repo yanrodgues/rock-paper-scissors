ee9af60e9717731c6da182aafffe7df1a1bc5a96
yanrodgues <yandeassis@pm.me>
2023-03-30 17:55:31 -0300

    Initial commmit

---
+++ app.js
@@ -0,0 +1,10 @@
+// function called getComputerChoice
+/// Get random choice between rock paper scissors
+
+// function called playRound 
+// takes playerSelection and computerSelection, and returns a string with who won
+
+// playerSellection must accept inputs case insensitively
+// takes input from player, and converts to lowercase
+
+// create a game() function and put 5 playRound functions inside
