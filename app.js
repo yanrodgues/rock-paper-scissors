0bef8c9abc75cb6f2fd3fa53ab870eaa6fd39b0d
yanrodgues <yandeassis@pm.me>
2023-03-30 20:06:57 -0300

    Add combinations check, and 5 rounds

--- app.js
+++ app.js
@@ -1,40 +1,107 @@
+// initialize variables for scores, selections and seed
 let playerSelection = 0;
-
 let computerSelection = 0;
-
 let playerScore = 0;
-
 let computerScore = 0;
-
 let seedComputer = 0;
 
-let rock = 1
-
-let paper = 2
-
-let scissors = 3
-
-// function called getComputerChoice
+//defines computer choice randomly
 function getComputerChoice() {
-    seedComputer = Math.floor(Math.random() * 100)
-    if (seedComputer <= 33){
-        computerSelection = rock
-    }else if (seedComputer <= 66){
-        computerSelection = paper
-    }else if (seedComputer >= 66){
-        computerSelection = scissors
-    }
+  seedComputer = Math.floor(Math.random() * 100);
+  if (seedComputer <= 33) {
+    computerSelection = "rock";
+  } else if (seedComputer <= 66) {
+    computerSelection = "paper";
+  } else if (seedComputer >= 66) {
+    computerSelection = "scissors";
+  }
 }
 
-getComputerChoice()
+// prompts the user for a selection
+function getPlayerChoice() {
+  playerSelection = prompt("Rock, Paper or Scissors?");
+  playerSelection = playerSelection.toLowerCase();
+  return playerSelection;
+}
 
-console.log(computerSelection);
-/// Get random choice between rock paper scissors
-
-// function called playRound
-// takes playerSelection and computerSelection, and returns a string with who won
-
-// playerSellection must accept inputs case insensitively
-// takes input from player, and converts to lowercase
+// function called playRound, and returns a string with who won
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    console.log("It's a tie!");
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    console.log("You lose!");
+    computerScore++;
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    console.log("You win!");
+    playerScore++;
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    console.log("You win!");
+    playerScore++;
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    console.log("You win!");
+    playerScore++;
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    console.log("You lose!");
+    computerScore++;
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    console.log("You lose!");
+    computerScore++;
+  }
+}
 
 // create a game() function and put 5 playRound functions inside
+function game() {
+
+  //match 1
+  getComputerChoice();
+
+  //take player selection
+  getPlayerChoice();
+
+  //check who won
+  playRound(playerSelection, computerSelection);
+
+  //match 2
+  getComputerChoice();
+
+  //take player selection
+  getPlayerChoice();
+
+  //check who won
+  playRound(playerSelection, computerSelection);
+
+  //match 3
+  getComputerChoice();
+
+  //take player selection
+  getPlayerChoice();
+
+  //check who won
+  playRound(playerSelection, computerSelection);
+
+  //match 4
+  getComputerChoice();
+
+  //take player selection
+  getPlayerChoice();
+
+  //check who won
+  playRound(playerSelection, computerSelection);
+
+  //match 5
+  getComputerChoice();
+
+  //take player selection
+  getPlayerChoice();
+
+  //check who won
+  playRound(playerSelection, computerSelection);
+
+  if(playerScore > computerScore){
+    console.log("You win the game");
+  }else console.log("You lose the game")
+
+}
+
+game();
+console.log(computerSelection)
