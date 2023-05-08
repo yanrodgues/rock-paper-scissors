40d74e8b0329bce535b9c9c15c248638fddd2a08
Yan Rodrigues <yandeassis@pm.me>
2023-05-07 21:08:35 -0300

    Add UI

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let computerSelection = 0;
 let playerScore = 0;
 let computerScore = 0;
 let seedComputer = 0;
+const divResultadoPartial = document.querySelector("#partialResults");
 
 //defines computer choice randomly
 function getComputerChoice() {
@@ -26,80 +27,119 @@ function getPlayerChoice() {
 
 // function called playRound, and returns a string with who won
 function playRound(playerSelection, computerSelection) {
+  divResultado = document.querySelector(".results");
+  
+  if ((playerScore < 5) && (computerScore  < 5)) {
+    
   if (playerSelection === computerSelection) {
-    console.log("It's a tie!");
+    divResultado.textContent = "Empate"
   } else if (playerSelection == "rock" && computerSelection == "paper") {
-    console.log("You lose!");
+    divResultado.textContent = "Você perdeu!"
     computerScore++;
   } else if (playerSelection == "rock" && computerSelection == "scissors") {
-    console.log("You win!");
+    divResultado.textContent = "Você venceu!"
     playerScore++;
   } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    console.log("You win!");
+    divResultado.textContent = "Você venceu!"
     playerScore++;
   } else if (playerSelection == "paper" && computerSelection == "rock") {
-    console.log("You win!");
+    divResultado.textContent = "Você venceu!"
     playerScore++;
   } else if (playerSelection == "paper" && computerSelection == "scissors") {
-    console.log("You lose!");
+    divResultado.textContent = "Você perdeu!"
     computerScore++;
   } else if (playerSelection == "scissors" && computerSelection == "rock") {
-    console.log("You lose!");
+    divResultado.textContent = "Você perdeu!"
     computerScore++;
   }
+} else divResultadoPartial.innerHTML = `O computador tem  ${computerScore} pontos e o jogador tem ${playerScore}`;
+
+divResultadoPartial.innerHTML = `O computador tem  ${computerScore} pontos e o jogador tem ${playerScore}`;
+
+  
 }
+
+
+
+
+const btnPedra = document.querySelector("#pedra");
+
+btnPedra.addEventListener('click', () => {
+  getComputerChoice();
+  playerSelection = "rock";
+  playRound(playerSelection, computerSelection);
+
+})
+
+const btnPapel = document.querySelector("#papel");
+
+btnPapel.addEventListener('click', () => {
+  getComputerChoice();
+  playerSelection = "paper";
+  playRound(playerSelection, computerSelection);
+})
+
+const btnTesoura = document.querySelector("#tesoura");
+
+btnTesoura.addEventListener('click', () => {
+  getComputerChoice();
+  playerSelection = "scissors";
+  playRound(playerSelection, computerSelection);
+})
+
+
 
 // create a game() function and put 5 playRound functions inside
-function game() {
-  //match 1
-  getComputerChoice();
+// function game() {
+//   //match 1
+//   getComputerChoice();
 
-  //take player selection
-  getPlayerChoice();
+//   //take player selection
+//   getPlayerChoice();
 
-  //check who won
-  playRound(playerSelection, computerSelection);
+//   //check who won
+//   playRound(playerSelection, computerSelection);
 
-  //match 2
-  getComputerChoice();
+//   //match 2
+//   getComputerChoice();
 
-  //take player selection
-  getPlayerChoice();
+//   //take player selection
+//   getPlayerChoice();
 
-  //check who won
-  playRound(playerSelection, computerSelection);
+//   //check who won
+//   playRound(playerSelection, computerSelection);
 
-  //match 3
-  getComputerChoice();
+//   //match 3
+//   getComputerChoice();
 
-  //take player selection
-  getPlayerChoice();
+//   //take player selection
+//   getPlayerChoice();
 
-  //check who won
-  playRound(playerSelection, computerSelection);
+//   //check who won
+//   playRound(playerSelection, computerSelection);
 
-  //match 4
-  getComputerChoice();
+//   //match 4
+//   getComputerChoice();
 
-  //take player selection
-  getPlayerChoice();
+//   //take player selection
+//   getPlayerChoice();
 
-  //check who won
-  playRound(playerSelection, computerSelection);
+//   //check who won
+//   playRound(playerSelection, computerSelection);
 
-  //match 5
-  getComputerChoice();
+//   //match 5
+//   getComputerChoice();
 
-  //take player selection
-  getPlayerChoice();
+//   //take player selection
+//   getPlayerChoice();
 
-  //check who won
-  playRound(playerSelection, computerSelection);
+//   //check who won
+//   playRound(playerSelection, computerSelection);
 
-  if (playerScore > computerScore) {
-    console.log("You win the game");
-  } else console.log("You lose the game");
-}
+//   if (playerScore > computerScore) {
+//     console.log("You win the game");
+//   } else console.log("You lose the game");
+// }
 
-game();
-console.log(computerSelection);
+// game();
+// console.log(computerSelection);
